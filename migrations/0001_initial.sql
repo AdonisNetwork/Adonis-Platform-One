397ad78aafc5602ab7a8291d5bd8ef958b0c865b
Serkan Kaplan <104901989+dr-Serkan-Kaplan@users.noreply.github.com>
2026-01-21 11:37:28 +0300

    Create jobs table in initial migration

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE jobs (
+    id UUID PRIMARY KEY,
+    type VARCHAR(255),
+    payload JSONB,
+    status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
